f10776608f77de21d62782343034decfc37c7469
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2026-06-04 22:34:38 +0500

    create QuantityCounter

--- src/components/QuantityCounter.js
+++ src/components/QuantityCounter.js
@@ -1,9 +1,28 @@
-export default function QuantityCounter() {
+import { useDispatch } from "react-redux"
+import { quantityCounter } from "../store/actions";
+
+export default function QuantityCounter({ quantity, id }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="card-price-count">
-      <button className="card-price-count-btn">-</button>
-      <span className="card-price-count-value">0</span>
-      <button className="card-price-count-btn">+</button>
+
+      <button 
+        className="card-price-count-btn" 
+        onClick={() => dispatch(quantityCounter(quantity - 1, id))}
+      >
+        -
+      </button>
+
+      <span className="card-price-count-value">{quantity}</span>
+
+      <button
+        className="card-price-count-btn"
+        onClick={() => dispatch(quantityCounter(quantity + 1, id))}
+      >
+        +
+      </button>
+
     </div>
   )
 }
