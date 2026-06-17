8a0fcc6f77912665b51e8c4a774751e17a9df1f6
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2026-06-17 15:54:13 +0500

    update cartProductReducer

--- src/store/reducers/cartProductReducer.js
+++ src/store/reducers/cartProductReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART_SUCCEEDED, REMOVE_FROM_CART, UPDATE_CART_PRODUCT_QUANTITY } from "../types";
+import { ADD_TO_CART_SUCCEEDED, REMOVE_FROM_CART, UPDATE_PRODUCT_QUANTITY } from "../types";
 
 const initialState = {
   products: [],
@@ -12,8 +12,8 @@ export function cartProductReducer(state = initialState, action) {
     case REMOVE_FROM_CART:
         return { ...state, products: state.products.filter((product) => product.id !== action.payload) };
 
-    case UPDATE_CART_PRODUCT_QUANTITY:
-      return { ...state, products: state.products.map((product) => product.id === action.payload.id ? { ...product, quantity: action.payload.quantity } : product) };
+    case UPDATE_PRODUCT_QUANTITY:
+      return { ...state, products: [...state.products, action.payload]};
 
     default:
       return state;
