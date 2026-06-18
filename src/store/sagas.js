9aad4513411c0448c64655ec73ec39e70fa97ffb
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2026-06-18 21:03:49 +0500

    update sagas

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,6 +1,6 @@
 import { all, call, fork, put, select, takeEvery, takeLatest } from 'redux-saga/effects';
-import { THEME_SET, MODAL_CLOSE, MODAL_TOGGLE, PRODUCTS_FETCH_REQUESTED, ADD_TO_CART } from './types';
-import { fetchProductsSucceeded, fetchProductsFailed, fetchProductsToCartFailed, addToCartSucceeded } from './actions';
+import { THEME_SET, MODAL_CLOSE, MODAL_TOGGLE, PRODUCTS_FETCH_REQUESTED, ADD_TO_CART, UPDATE_CART_PRODUCT_QUANTITY } from './types';
+import { fetchProductsSucceeded, fetchProductsFailed, fetchProductsToCartFailed, addToCartSucceeded, quantityUpdate } from './actions';
 import { fetchProducts } from '../services/productsApi';
 import { THEME_STORAGE_KEY } from './theme';
 import { writeStoredModalOpen } from './modal';
@@ -50,7 +50,7 @@ function* addToCartWorker(action) {
     const id = action.payload;
     const { products } = yield select((state) => state.products.data);
     const product = products.find((product) => product.id === id);
-    yield put(addToCartSucceeded({ ...product, isToCart: true }));
+    yield put(addToCartSucceeded(product));
   } catch (err) {
     yield put(fetchProductsToCartFailed(err.message));
   }
@@ -62,11 +62,26 @@ function* addToCartWatcher() {
 
 
 
+
+function* quantityCounterWorker(action) {
+  const { id } = action.payload;
+  const products = yield select((state) => state.cart.products);
+  const product = products.find((product) => product.id === id);
+  yield put(quantityUpdate(product))
+}
+
+function* quantityCounterWatcher() {
+  yield takeEvery(UPDATE_CART_PRODUCT_QUANTITY, quantityCounterWorker)
+}
+
+
+
 export default function* rootSaga() {
   yield all([
     fork(productsWatcher),
     fork(themePersistenceWatcher),
     fork(modalPersistenceWatcher),
     fork(addToCartWatcher),
+    fork(quantityCounterWatcher),
   ]);
 }
