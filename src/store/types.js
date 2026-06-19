51dfa3aa6d70ae012d267264854b98a14d6123c5
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2026-06-19 15:14:03 +0500

    update types

--- src/store/types.js
+++ src/store/types.js
@@ -14,3 +14,4 @@ export const ADD_TO_CART_SUCCEEDED = 'cart/addToCartSucceeded';
 export const PRODUCTS_TO_CART_FAILED = 'cart/productsToCartFailed';
 export const REMOVE_FROM_CART = 'cart/removeFromCart';
 export const UPDATE_CART_PRODUCT_QUANTITY = 'cart/updateCartProductQuantity';
+export const UPDATE_PRODUCT_QUANTITY = 'cart/updateProductQuantity'
